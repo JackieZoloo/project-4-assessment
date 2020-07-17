908809c3960dc6b19823f02e291cb38d7a607446
jackiezoloo <62490560+JackieZoloo@users.noreply.github.com>
2020-07-17 16:43:08 -0600

    Updates

---
+++ precache-manifest.5939ada540d7bef9c703bd5bdf05185f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddd740b5fa1b8524d623c72b6498182",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "92e2d1e28aa39b037f7c",
+    "url": "/project-4-assessment/static/css/main.47f89c5d.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e2d1e28aa39b037f7c",
+    "url": "/project-4-assessment/static/js/main.d243ac68.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
